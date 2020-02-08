234819a726b867e8f1daf1f89b0e82e808378304
uriegel <uriegel@hotmail.de>
2020-02-08 12:52:30 +0100

    UwebServer integrated

--- Server/Program.fs
+++ Server/Program.fs
@@ -1,8 +1,15 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿let configuration = Configuration.create {
+    Configuration.createEmpty() with 
+        WebRoot = "/home/uwe/projects/UwebServer/webroot" 
+        Port = 20000
+        AllowOrigins = Some [| "http://localhost:8080" |]
+        favicon = "Uwe.jpg"
+}
 
-open System
-
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+try 
+    let server = Server.create configuration 
+    server.start ()
+    stdin.ReadLine() |> ignore
+    server.stop ()
+with
+    | ex -> printfn "Fehler: %O" ex
