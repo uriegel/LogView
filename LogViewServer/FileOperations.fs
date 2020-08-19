cdae4b30f1318a52202cd2185133c501aaee94e5
Uwe Riegel <uriegel@hotmail.de>
2020-08-19 06:57:47 +0200

    Convertes form lineIndexes to lines

--- LogViewServer/FileOperations.fs
+++ LogViewServer/FileOperations.fs
@@ -4,15 +4,28 @@ open System.Text
 open FSharpTools
 open FSharpTools.String
 open System.Globalization
+open System
+
+type Line = {
+    Pos: int64
+    Length: int
+}
 
 type FileOperations(path: string, formatMilliseconds: bool, utf8: bool) = 
     let mutable fileSize = 0L
+    let buffer = Array.zeroCreate 200000    
+    let encoding = if utf8 then Encoding.UTF8 else Encoding.GetEncoding (CultureInfo.CurrentCulture.TextInfo.ANSICodePage)
 
     let accessfile adjustLength = 
         let file = new FileStream (path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
         if adjustLength then
             fileSize <-file.Length
         file
+
+    let getString (file: Stream) line =
+        file.Position <- line.Pos
+        file.Read (buffer, 0, line.Length) |> ignore
+        encoding.GetString (buffer, 0, line.Length)
 
     let createLogIndexes (file: FileStream) =
         let buffer = Array.zeroCreate 20000
@@ -39,43 +52,57 @@ type FileOperations(path: string, formatMilliseconds: bool, utf8: bool) =
                 else
                     None
 
-            let ret = 
+            let indexes = 
                 0 |> Seq.unfold (fun _ ->
                     match getLinesBuffer () with
                     | Some lines -> Some (lines, 0)
-                    | None -> None) |> Seq.concat
-            ret
+                    | None -> None) 
+                |> Seq.concat
+                |> Seq.toArray
 
-        getLines ()
+            seq { for i in 1 .. indexes.Length - 1 -> { Pos = indexes.[i-1]; Length = int (indexes.[i] - indexes.[i-1]) }}                
+            |> Seq.toArray
+
+        let lineIndexes = getLines ()
+
+        printfn "get start"
+
+        let getString = getString file
+        let endIndex = lineIndexes.Length - 1
+
+        let filter line = 
+            let text = getString line 
+            match text |> String.indexOf "Refreshing User Cache" with
+            | Some v -> true
+            | None -> false
+
+        let lineIndexes = 
+            lineIndexes 
+            |> Array.filter filter
+
+        printfn "get end"
+
+        lineIndexes
+
+
+
 
     let mutable lineIndexes =
         use file = accessfile true 
         file
         |> createLogIndexes 
-        |> Seq.toArray    
 
     let getLines (file: Stream) startIndex endIndex =
         let posType = if formatMilliseconds then 24 else 20
         let timeLength = if formatMilliseconds then 12 else 8
         let textPos = if formatMilliseconds then 29 else 25
 
-        let buffer = Array.zeroCreate 200000    
-        let getString startPos length =
-            file.Position <- startPos
-            file.Read (buffer, 0, length) |> ignore
-            let encoding = if utf8 then Encoding.UTF8 else Encoding.GetEncoding (CultureInfo.CurrentCulture.TextInfo.ANSICodePage)
-            encoding.GetString (buffer, 0, length)
-
-        let getNextIndex i =
-            if i < lineIndexes.Length - 1 then 
-                lineIndexes.[i+1] 
-            else 
-                fileSize
+        let getString = getString file
 
         let endIndex = if endIndex < lineIndexes.Length then endIndex else lineIndexes.Length - 1
-        seq { for i in startIndex .. endIndex -> (lineIndexes.[i], getNextIndex i, i) }
-        |> Seq.map (fun (n, m, i) -> 
-            let text = getString n (int (m - n))
+        seq { for i in startIndex .. endIndex -> (lineIndexes.[i], i) }
+        |> Seq.map (fun (n, i) -> 
+            let text = getString n
 
             let getItemParts text msgType =
                 if msgType <> MsgType.NewLine then 
