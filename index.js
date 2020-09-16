3349e32c68d8f371df4f6913703c92cf05b79242
Uwe Riegel <uriegel@hotmail.de>
2020-09-16 11:23:06 +0200

    Install with function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import EventLogView from "./EventLogView.vue"
 //import TableView from 'virtual-table-vue'
 
 export default {
-    install(Vue, options) {
+    install: function(Vue, options) {
         // Let's register our component globally
         // https://vuejs.org/v2/guide/components-registration.html
         Vue.component("event-log-view", EventLogView)
